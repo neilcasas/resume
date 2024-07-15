77018cfe23605ac75a08ddd65f88f8daecc24e3d
Neil Casas <neilalfonzcasas@gmail.com>
2024-07-15 20:53:21 +0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import "./styles/index.css";
+
+function App() {
+  return (
+    <>
+      <main>
+        <div className="control"></div>
+        <div className="cv-container"></div>
+      </main>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/PersonalDetails.jsx
@@ -0,0 +1,7 @@
+const PersonalDetails = () => {
+  return (
+    <div class="editor-inputs">
+      <h1>P</h1>
+    </div>
+  );
+};

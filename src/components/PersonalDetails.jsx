4ea05829916bd3e43a304593bec8e3fe2fe0ef38
Neil Casas <neilalfonzcasas@gmail.com>
2024-07-15 21:18:16 +0800

    created personal details component

--- src/components/PersonalDetails.jsx
+++ src/components/PersonalDetails.jsx
@@ -1,7 +1,17 @@
 const PersonalDetails = () => {
   return (
-    <div class="editor-inputs">
-      <h1>P</h1>
+    <div className="editor-inputs">
+      <h1>Personal Details</h1>
+      <div className="input-group">
+        <div className="name-input">
+          <label htmlFor="name">Full Name</label>
+          <input type="text" id="name" name="name" />
+        </div>
+        <div className="email-input">
+          <label htmlFor="email">Email</label>
+          <input type="text" id="email" name="email" />
+        </div>
+      </div>
     </div>
   );
 };

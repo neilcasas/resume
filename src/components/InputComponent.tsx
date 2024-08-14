47269f8bbed8847aeb47b7fc708254206e1c80f3
Neil Casas <neilalfonzcasas@gmail.com>
2024-08-14 22:23:24 +0800

    created input component

---
+++ src/components/InputComponent.tsx
@@ -0,0 +1,11 @@
+interface InputComponentProps {
+    title: string;
+    id: string;
+    type: "text"|"email"|"number"|"date";
+    placeholder: string;
+}
+
+function InputComponent({props: InputComponentProps}) {
+    return (
+    )
+}

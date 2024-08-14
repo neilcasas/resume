5a9afc63a5e8fd2a83651a07403d098f93c799c0
Neil Casas <neilalfonzcasas@gmail.com>
2024-08-15 00:03:02 +0800

    added classnames and rremoved app.scss redundant import

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -4,10 +4,16 @@ import Education from "./Education";
 
 function Editor() {
   return (
-    <div>
+    <div className="editor-container">
       <General />
-      <Experience />
-      <Education />
+      <div className="exp-container">
+        <h1>Experience</h1>
+        <Experience />
+      </div>
+      <div className="educ-container">
+        <h1>Education</h1>
+        <Education />
+      </div>
     </div>
   );
 }

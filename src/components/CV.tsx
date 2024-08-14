ba63105536e74ba8426c595ed8e39ada6e001ddc
Neil Casas <neilalfonzcasas@gmail.com>
2024-08-15 00:36:01 +0800

    added initial styling to CV component

--- src/components/CV.tsx
+++ src/components/CV.tsx
@@ -1,20 +1,23 @@
+import "../styles/CV.scss";
 function CV() {
   return (
     <div className="cv-container">
       <div className="cv-main">
-        <div className="cv-details">
-          <h1>John Doe</h1>
-          <div className="cv-contact">
-            <div>johndoe@email.com</div>
-            <div>+63 918 647 1576</div>
-            <div>London, UK</div>
+        <div className="cv-content">
+          <div className="cv-details">
+            <h1>John Doe</h1>
+            <div className="cv-contact">
+              <div>johndoe@email.com</div>
+              <div>+63 918 647 1576</div>
+              <div>London, UK</div>
+            </div>
           </div>
-        </div>
-        <div className="cv-education">
-          <div>University</div>
-        </div>
-        <div className="cv-experience">
-          <div>Software Engineer</div>
+          <div className="cv-education">
+            <div>University</div>
+          </div>
+          <div className="cv-experience">
+            <div>Software Engineer</div>
+          </div>
         </div>
       </div>
     </div>

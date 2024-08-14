be39d8434048198c9789586a50948da73f8a2874
Neil Casas <neilalfonzcasas@gmail.com>
2024-08-15 00:31:18 +0800

    added initial styling to App.scss for initial layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
+import CV from "./components/CV";
 import Editor from "./components/Editor";
 import "./styles/App.scss";
 
 function App() {
   return (
-    <>
+    <div className="main">
       <Editor />
-    </>
+      <CV />
+    </div>
   );
 }
 

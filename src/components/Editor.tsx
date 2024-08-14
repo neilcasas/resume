c7a78af396f3ecb855d7f2503627d471b0c2505d
Neil Casas <neilalfonzcasas@gmail.com>
2024-08-14 23:18:56 +0800

    added components to editor component

---
+++ src/components/Editor.tsx
@@ -0,0 +1,15 @@
+import General from "./General";
+import Experience from "./Experience";
+import Education from "./Education";
+
+function Editor() {
+  return (
+    <div>
+      <General />
+      <Experience />
+      <Education />
+    </div>
+  );
+}
+
+export default Editor;

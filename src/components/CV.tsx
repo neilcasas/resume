aefd66bed75b8d421ebcdc4b7849473d166f61a1
Neil Casas <neilalfonzcasas@gmail.com>
2024-08-15 00:24:43 +0800

    added initial elements to CV component

---
+++ src/components/CV.tsx
@@ -0,0 +1,24 @@
+function CV() {
+  return (
+    <div className="cv-container">
+      <div className="cv-main">
+        <div className="cv-details">
+          <h1>John Doe</h1>
+          <div className="cv-contact">
+            <div>johndoe@email.com</div>
+            <div>+63 918 647 1576</div>
+            <div>London, UK</div>
+          </div>
+        </div>
+        <div className="cv-education">
+          <div>University</div>
+        </div>
+        <div className="cv-experience">
+          <div>Software Engineer</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default CV;

e8b559156f84f358be513374665788d928684fcc
Neil Casas <neilalfonzcasas@gmail.com>
2024-08-14 22:44:34 +0800

    added input component to general component

--- src/components/General.tsx
+++ src/components/General.tsx
@@ -1,5 +1,32 @@
 import InputComponent from "./InputComponent";
 
 function General() {
-    <InputComponent title="Full Name" id="name" type="text" placeholder="e.g. LeBron James"/>
+  <div className="card">
+    <InputComponent
+      title="Full Name"
+      id="name"
+      type="text"
+      placeholder="e.g. LeBron James"
+    />
+    <InputComponent
+      title="Email"
+      id="email"
+      type="email"
+      placeholder="e.g. yourname@email.com"
+    />
+    <InputComponent
+      title="Phone number"
+      id="phone"
+      type="text"
+      placeholder="e.g. +63 917 508 1259"
+    />
+    <InputComponent
+      title="Address"
+      id="address"
+      type="text"
+      placeholder="e.g. Manila, Philippines"
+    />
+  </div>;
 }
+
+export default General;

33c4f3f2c069eead7ae1747ea9ba549ab107746c
Neil Casas <neilalfonzcasas@gmail.com>
2024-08-14 22:16:12 +0800

    replaced css files with scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import './App.scss'
 
 function App() {
 
   return (
     <>
+    <h1>Test</h1>
     </>
   )
 }

dae0baf80ca24002df2f7d4d8a85183d27a22b32
Neil Casas <neilalfonzcasas@gmail.com>
2024-08-14 22:59:41 +0800

    added input components to experience component

---
+++ src/components/Experience.tsx
@@ -0,0 +1,43 @@
+import InputComponent from "./InputComponent";
+
+function Experience() {
+  return (
+    <div className="card">
+      <InputComponent
+        title="Company Name"
+        id="company"
+        type="text"
+        placeholder="e.g. Umbrella Inc."
+      />
+      <InputComponent
+        title="Position Title"
+        id="position"
+        type="text"
+        placeholder="e.g. Software Engineer"
+      />
+      <div className="exp-date">
+        <InputComponent
+          title="Start Date"
+          id="exp-start"
+          type="date"
+          placeholder=""
+        />
+        <InputComponent
+          title="End Date"
+          id="exp-end"
+          type="date"
+          placeholder=""
+        />
+      </div>
+      <InputComponent
+        title="Location"
+        id="location"
+        type="text"
+        placeholder="e.g. Manila, Philippines"
+      />
+      <textarea className="exp-description"></textarea>;
+    </div>
+  );
+}
+
+export default Experience;

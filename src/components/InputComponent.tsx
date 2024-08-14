4ce633c51c5260389e219ace5ab2c252aae17802
Neil Casas <neilalfonzcasas@gmail.com>
2024-08-14 22:28:20 +0800

    finished input component

--- src/components/InputComponent.tsx
+++ src/components/InputComponent.tsx
@@ -5,7 +5,13 @@ interface InputComponentProps {
     placeholder: string;
 }
 
-function InputComponent({props: InputComponentProps}) {
-    return (
+function InputComponent(props: InputComponentProps) {
+    return (  
+    <div className="mb-3">
+        <label htmlFor={props.id} className="form-label">{props.title}</label>
+        <input type={props.type} className="form-control" id={props.id} placeholder={props.placeholder}/>
+    </div>
     )
 }
+
+export default InputComponent;

1ff1d4712f713dbffbe55c4bbb34a4175b179b97
Neil Casas <neilalfonzcasas@gmail.com>
2024-08-17 13:48:04 +0800

    added index to mapped experience and education components

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -4,20 +4,27 @@ import Education from "./Education";
 import { CVProps } from "./CV";
 import "../styles/Editor.scss";
 
-function Editor(props: CVProps) {
+// Interface containing the update function for each part of the CV
+interface EditorProps extends CVProps {
+  updateGeneral: (general: CVProps["general"]) => void;
+  updateEducation: (education: CVProps["education"]) => void;
+  updateExperience: (experience: CVProps["experience"]) => void;
+}
+
+function Editor(props: EditorProps) {
   return (
     <div className="editor-container">
       <General {...props.general} />
       <div className="exp-container">
         <h1>Experience</h1>
-        {props.experience.map((exp) => (
-          <Experience {...exp} />
+        {props.experience.map((exp, index) => (
+          <Experience key={index} {...exp} />
         ))}
       </div>
       <div className="educ-container">
         <h1>Education</h1>
-        {props.education.map((educ) => (
-          <Education {...educ} />
+        {props.education.map((educ, index) => (
+          <Education key={index} {...educ} />
         ))}
       </div>
     </div>
